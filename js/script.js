92d8aaedb927f24e87d27b9892cf06c3683bcf77
tinuola <24995224+tinuola@users.noreply.github.com>
2023-02-16 18:08:48 -0800

    Slight styling change to footer

--- js/script.js
+++ js/script.js
@@ -11,20 +11,20 @@ const cardByline = document.querySelector('.card-byline-label')
 
 const shareIconGroup = document.querySelector('.share-icon-group')
 
-  shareIcon.addEventListener('click', function(){
-    // Change share icon background
-    this.classList.toggle('share-icon--bground')
+shareIcon.addEventListener('click', function(){
+  // Change share icon background
+  this.classList.toggle('share-icon--bground')
 
-    // Swap icons
-    iconDark.classList.toggle('hide-icon')
-    iconLight.classList.toggle('show-icon')
+  // Swap icons
+  iconDark.classList.toggle('hide-icon')
+  iconLight.classList.toggle('show-icon')
 
-    // Change footer background (mobile)
-    cardFooter.classList.toggle('card-footer-dark')
+  // Change footer background (mobile)
+  cardFooter.classList.toggle('card-footer-dark')
 
-    // Hide/Show author label (mobile)
-    cardByline.classList.toggle('hide-byline-label')
-    
-    // Hide/Show icon group
-    shareIconGroup.classList.toggle('show-icon-group')
-  })
+  // Hide/Show author label (mobile)
+  cardByline.classList.toggle('hide-byline-label')
+  
+  // Hide/Show icon group
+  shareIconGroup.classList.toggle('show-icon-group')
+})
